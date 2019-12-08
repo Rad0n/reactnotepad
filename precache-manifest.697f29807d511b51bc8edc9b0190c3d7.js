4047b28b674f0d252b856e159b105f358c7c9035
Pradyumna Mahajan <pradyumnamahajan@protonmail.com>
2019-12-08 06:48:11 +0530

    Updates

---
+++ precache-manifest.697f29807d511b51bc8edc9b0190c3d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ecd1ea79d197490ebca2bef0877f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5903683b6fb4e62c712",
+    "url": "/static/css/main.3137371d.chunk.css"
+  },
+  {
+    "revision": "3622a250955b2be55f31",
+    "url": "/static/js/2.ce90bbee.chunk.js"
+  },
+  {
+    "revision": "a5903683b6fb4e62c712",
+    "url": "/static/js/main.f0d3a9f8.chunk.js"
+  },
+  {
+    "revision": "82c10ab8d1e53e3e1db0",
+    "url": "/static/js/runtime-main.caac3b5a.js"
+  }
+]);
